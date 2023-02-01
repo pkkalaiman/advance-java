94699169dd3048d63b739e6b21a51b84668729b3
pkkalaiman <pkmanixworkz@gmail.com>
2023-02-01 20:41:29 +0530

    new task

--- food/src/main/java/com/xworkz/food/configuration/FoodInit.java
+++ food/src/main/java/com/xworkz/food/configuration/FoodInit.java
@@ -1,45 +1,39 @@
-package com.xworkz.manilazer.configuration;
+package com.xworkz.food.configuration;
 
-import java.util.Arrays;
+import java.util.Arrays; 
 
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class SpringWebInit extends AbstractAnnotationConfigDispatcherServletInitializer implements WebMvcConfigurer {
-
-	public SpringWebInit() {
-		System.out.println("Created :" + this.getClass().getSimpleName());
-	}
+public class FoodInit extends AbstractAnnotationConfigDispatcherServletInitializer implements WebMvcConfigurer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		System.out.println("Running getRootConfigClasses");
+		System.out.println("Running in getRootConfiguration");
 		return null;
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		System.out.println("Running getServletConfigClasses");
-		Class[] configClass= {SpringBeanConfiguration.class};
-		
-		return configClass;
+		System.out.println("Running in getServletCongiguration");
+		Class[] configure = { FoodConfiguration.class };
+		return configure;
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		System.out.println("Running getServletMappings");
-		String[] ref= {"/"};
-		System.out.println("getServletMapping :"+Arrays.toString(ref));
+		System.out.println("Running in getServletMappings");
+		String[] ref = { "/" };
+		System.out.println("configuration :" + Arrays.toString(ref));
 		return ref;
 	}
-	
+
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		System.out.println("Running in ConfigureDefaultServletHandling");
 		WebMvcConfigurer.super.configureDefaultServletHandling(configurer);
+
 		configurer.enable();
 	}
-	
-	
-	
 }
